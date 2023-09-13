ebb03176cd12b0674b6c96276d93276461b5937a
ali-bennezia <bennezia.ali@gmail.com>
2023-09-13 22:53:59 +0200

    initialization & loop setup

---
+++ build.bat
@@ -0,0 +1,15 @@
+echo off
+echo "Compiling ..."
+
+gcc -o ./bin/asciidungeon.exe ./src/main.c ./src/utils.c -I"./../asciigl/include" -L"./libs" -lasciigl-static
+
+if not exist bin (
+	mkdir bin
+)
+if not exist assets (
+	mkdir assets
+)
+
+xcopy /e /i /y /q assets "bin/assets"
+
+echo "Compilation done."
--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <asciigl.h>
 
+static void init()
+{
+	asciigl_init();
+}
+
+static void loop()
+{
+	while (1){
+		asciigl_process_frame();
+	}
+}
 
 int main( int argc, char **argv )
 {
-
+	init();
+	loop();
+	
 	return 0;
 }
