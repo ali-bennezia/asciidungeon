6435309038398d5ca5eb06fe9aaaba4352218f6f
ali-bennezia <bennezia.ali@gmail.com>
2024-08-24 20:28:38 +0200

    bounding box update function

--- src/pseudophysics.c
+++ src/pseudophysics.c
@@ -43,8 +43,41 @@ void asciidng_generate_parallelepiped_collider_vertices( Collider collider, fvec
 
 void asciidng_process_transform_bounding_box( RigidBody *rigid_body )
 {
+	Transform transform = rigid_body->transform;
+
 	fvec3 vertices[ 8 ];
-	
+	asciidng_generate_parallelepiped_collider_vertices( rigid_body->collider, vertices );
+
+	fmat3 rotation_matrix;
+	fmat3_rotation_matrix( transform.rotation.x, transform.rotation.y, transform.rotation.z, rotation_matrix );
+
+	float min_x, min_y, min_z, max_x, max_y, max_z;
+
+	for ( size_t i = 0; i < 8; ++i ){
+		vertices[ i ] = fmat3_fvec3_mult( rotation_matrix, vertices[ i ] );
+		if ( i > 0 ){
+			if ( min_x > vertices[ i ].x ) min_x = vertices[ i ].x;
+			if ( min_y > vertices[ i ].y ) min_y = vertices[ i ].y;
+			if ( min_z > vertices[ i ].z ) min_z = vertices[ i ].z;
+
+			if ( max_x < vertices[ i ].x ) max_x = vertices[ i ].x;
+			if ( max_y < vertices[ i ].y ) max_y = vertices[ i ].y;
+			if ( max_z < vertices[ i ].z ) max_z = vertices[ i ].z;
+		} else {
+			min_x = vertices[ i ].x;
+			min_y = vertices[ i ].y;
+			min_z = vertices[ i ].z;
+
+			max_x = vertices[ i ].x;
+			max_y = vertices[ i ].y;
+			max_z = vertices[ i ].z;
+		} 
+	}
+
+	fvec3 bb_min = { min_x, min_y, min_z }, bb_max = { max_x, max_y, max_z };
+
+	rigid_body->bounding_box_min = bb_min;
+	rigid_body->bounding_box_max = bb_max;
 }
 
 void asciidng_init_physics()
